96886f2168272122d00bf79ba934a235b336cfcf
Travis Mingalone <travismingalone@gmail.com>
2019-02-09 16:42:11 -0700

    Inital design for FCC Random Quote Generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quote: '',
+      author: ''
+    };
+  };
+
+  HandleClick() {
+    fetch('https://cors.io/?https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')
+    .then(results => results.json())
+    .then(data => {
+      console.log(data);
+      this.setState({quote: data.quoteText, author: data.quoteAuthor})
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  componentDidMount() {
+    fetch('https://cors.io/?https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')
+    .then(results => results.json())
+    .then(data => {
+      console.log(data);
+      this.setState({quote: data.quoteText, author: data.quoteAuthor})
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <div id="quote-box">
+        <p id='text'>
+        {this.state.quote}
+        </p>
+        <p id='author'>
+        {this.state.author}
+        </p>
+        <div className='buttons'>
+        <a className='button' href='twitter.com/intent/tweet' id="tweet-quote"><i className='fab fa-twitter-square fa-lg' id='tweet'></i></a>
+        <button className='button' onClick={() => this.HandleClick()} id="new-quote">New Quote</button>
+        </div>
+        </div>
         </header>
       </div>
     );
